5494ec993309e99a1ea6903d90b8344658be8090
jayasupriyasomasani <jayasupriya.somasani@narayanagroup.com>
2023-04-24 16:43:04 +0530

    initial commit

---
+++ lib/utils/colors.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+
+class AppColors {
+  static const textColor = Color(0xFFccc7c5);
+  static const mainColor = Color(0xFF89DAD0);
+  static const iconColor1 = Color(0xFFFFD28D);
+  static const iconColor2 = Color(0xFFFCAB88);
+  static const paraColor = Color(0xFF8F837F);
+  static const buttonBgColor = Color(0xFFf7f6f4);
+  static const signColor = Color(0xFFA9A29F);
+  static const titleColor = Color(0xFF5C524F);
+  static const mainBlackColor = Color(0xFF332D2B);
+  static const yellowColor = Color(0xFFFFD379);
+
+}
